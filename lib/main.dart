9c49b3aad35b7b6b72ee4c51ef6c2490f62c4787
katariya dharmesh <katariyadharmesh658@gmail.com>
2023-03-22 12:05:21 +0530

    getx calculator

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,8 @@ import 'controller/calculator_controller.dart';
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
